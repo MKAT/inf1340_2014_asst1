b4bcc877869c0b125cf0db77ce441cbddbc20204
avidhya <avidhya93@gmail.com>
2014-09-29 17:25:36 -0400

    Code for exercise2

--- exercise2.py
+++ exercise2.py
@@ -33,6 +33,40 @@ def checksum (upc):
     # check type of input
     # raise TypeError if not string
 
+
+    if isinstance(upc,str):
+        print('Entered code is valid')
+    else:
+        raise TypeError('Entered code not of type string')
+    if (len(upc) != 12):
+        raise ValueError('UPC code does not contain 12 digits')
+    else:
+        digit1 = int(upc[0])
+        digit2 = int(upc[1])
+        digit3 = int(upc[2])
+        digit4 = int(upc[3])
+        digit5 = int(upc[4])
+        digit6 = int(upc[5])
+        digit7 = int(upc[6])
+        digit8 = int(upc[7])
+        digit9 = int(upc[8])
+        digit10 = int(upc[9])
+        digit11 = int(upc[10])
+        digit12 = int(upc[11])
+        odd_position = (digit1 + digit3 + digit5 + digit7 + digit9 + digit11) * 3
+        even_position = (digit2 + digit4 + digit6 + digit8 + digit10) + odd_position
+
+        result1 = even_position % 10
+        if (result1 % 10) != 0:
+            checksum = 10 - result1
+        else:
+            checksum = result1
+        if checksum == digit12:
+            return True
+        else:
+            return False
+
+
     # check length of string
     # raise ValueError if not 12
 
@@ -44,5 +78,5 @@ def checksum (upc):
 
     # return True if they are equal, False otherwise
 
-    return False
+
 
