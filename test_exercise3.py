7e15ca11cbff9a621e0b9e0c3937827bc16d87a8
MKAT <Melissa.tomko@mail.utoronto.ca>
2014-10-05 18:07:04 -0400

    code for exercise1

--- test_exercise3.py
+++ test_exercise3.py
@@ -5,9 +5,10 @@ from exercise3 import decide_rps
 
 
 def test_checksum():
-    """
+
+
     Inputs that are the correct format and length
-    """
+
     assert decide_rps("Rock", "Paper") == 2
     assert decide_rps("Scissors", "Scissors") == 0
     assert decide_rps("Rock", "Scissors") == 1
@@ -18,3 +19,20 @@ def test_checksum():
         decide_rps("Lion","Tiger")
         decide_rps("Deer","Rock")
 
+    """
+    #Player 1 Win Scenarios
+
+    assert decide_rps("Rock", "Scissors") == 1
+    assert decide_rps("Paper", "Rock") == 1
+    assert decide_rps("Scissors", "Paper") == 1
+
+    #Player 2 Wins Scenarios
+    assert decide_rps("Paper", "Scissors") == 2
+    assert decide_rps("Rock", "Paper") == 2
+    assert decide_rps("Scissors", "Rock") == 2
+
+    #Other Scenarios are a Tie
+    assert decide_rps("Scissors", "Scissors") == 0
+    assert decide_rps("Rock", "Rock") == 0
+    assert decide_rps("Paper", "Paper") == 0
+    """
