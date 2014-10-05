879c07e7b494b07f1fdaf50a319e61bd35b06c6c
avidhya <avidhya93@gmail.com>
2014-10-05 18:01:54 -0400

    additional test cases included

--- test_exercise3.py
+++ test_exercise3.py
@@ -12,4 +12,9 @@ def test_checksum():
     assert decide_rps("Scissors", "Scissors") == 0
     assert decide_rps("Rock", "Scissors") == 1
     # other tests
+    with pytest.raises(TypeError):
+        decide_rps(1.0,2.0)
+    with pytest.raises(ValueError):
+        decide_rps("Lion","Tiger")
+        decide_rps("Deer","Rock")
 
